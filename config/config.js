64022aec0e1fc6ca77d6f930d261587639bbb60a
Andrei <44devtests@gmail.com>
2024-08-07 23:47:43 +0300

    add seeder for admin user

--- config/config.js
+++ config/config.js
@@ -10,6 +10,7 @@ module.exports = {
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
     dialect: "postgres",
+    seederStorage: "sequelize",
   },
   test: {
     username: "root",
